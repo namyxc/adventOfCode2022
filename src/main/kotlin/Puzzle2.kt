a239377c17a88a58feed20b4b9182a168f73bd2d
Librecz Bernadett <librecz.bernadett@magnetbank.hu>
2022-12-02 17:10:47 +0100

    Day 2

--- src/main/kotlin/Puzzle2.kt
+++ src/main/kotlin/Puzzle2.kt
@@ -1,2 +1,42 @@
-class Puzzle2 {
+object Puzzle2 {
+    val points =  mapOf<String, Int>(
+        "AX" to 1 + 3, // Rock     - Rock
+        "AY" to 2 + 6, // Rock     - Paper
+        "AZ" to 3 + 0, // Rock     - Scissors
+
+        "BX" to 1 + 0, // Paper    - Rock
+        "BY" to 2 + 3, // Paper    - Paper
+        "BZ" to 3 + 6, // Paper    - Scissors
+
+        "CX" to 1 + 6, // Scissors - Rock
+        "CY" to 2 + 0, // Scissors - Paper
+        "CZ" to 3 + 3, // Scissors - Scissors
+    )
+
+    val suggested =  mapOf<String, String>(
+        "AX" to "Z", // Rock     - Lose
+        "AY" to "X", // Rock     - Draw
+        "AZ" to "Y", // Rock     - Win
+
+        "BX" to "X", // Paper    - Lose
+        "BY" to "Y", // Paper    - Draw
+        "BZ" to "Z", // Paper    - Win
+
+        "CX" to "Y", // Scissors - Lose
+        "CY" to "Z", // Scissors - Draw
+        "CZ" to "X", // Scissors - Win
+    )
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val input = readInput(Puzzle2::class.java.getResource("puzzle2.txt").readText())
+        val result1 = sumPoints(input)
+        println(result1)
+        val result2 = sumPoints2(input)
+        println(result2)
+    }
+
+    fun readInput(input: String) = input.split("\n").filter { it.isNotEmpty() }.map { it.split(" ") }
+    fun sumPoints(testArray: List<List<String>>) = testArray.map { it.joinToString("")}.map { points.get(it)!! }.sum()
+    fun sumPoints2(testArray: List<List<String>>) = testArray.map { it.get(0) + suggested.get(it.joinToString("")) }.map { points.get(it)!!  }.sum()
 }
--- src/test/kotlin/Puzzle2Test.kt
+++ src/test/kotlin/Puzzle2Test.kt
@@ -1,2 +1,44 @@
-public class Puzzle2Test {
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class Puzzle2Test: PuzzleTest() {
+    companion object {
+        val testArray = arrayListOf(
+            arrayListOf("A", "Y"),
+            arrayListOf("B", "X"),
+            arrayListOf("C", "Z")
+        )
+    }
+
+    @Test
+    fun `sum points with suggested item`() {
+        val points = Puzzle2.sumPoints(testArray)
+        val expectedResult = 15
+        Assertions.assertEquals(expectedResult, points) {
+            "$points should equal $expectedResult"
+        }
+    }
+
+    @Test
+    fun `sum points with suggested outcome`() {
+        val points = Puzzle2.sumPoints2(testArray)
+        val expectedResult = 12
+        Assertions.assertEquals(expectedResult, points) {
+            "$points should equal $expectedResult"
+        }
+    }
+
+    @Test
+    fun `input to arraylist`(){
+        val input = "A Y\n" +
+                "B X\n" +
+                "C Z\n"
+
+        val inputAsArraylist = Puzzle2.readInput(input)
+        val expectedResult = testArray
+        Assertions.assertEquals(expectedResult.size, inputAsArraylist.size)
+        expectedResult.forEachIndexed { index, chars ->
+            Assertions.assertArrayEquals(expectedResult[index].toArray(), inputAsArraylist[index].toTypedArray())
+        }
+    }
 }
